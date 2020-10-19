4645760a22f02cfa76e6ba8dc7b2c5a81dc52afd
juanferdiax <60932912+juanferdiax@users.noreply.github.com>
2020-10-19 19:29:06 +0200

    Lo que tenemos hasta ahora

---
+++ BBDD/bbdd.sql
@@ -0,0 +1,317 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 13-10-2020 a las 16:46:12
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bbdd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `ID_Cliente` int(11) NOT NULL,
+  `DNI` varchar(9) NOT NULL,
+  `Nombre` varchar(100) NOT NULL,
+  `Direccion` varchar(200) NOT NULL,
+  `Email` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `compras`
+--
+
+CREATE TABLE `compras` (
+  `ID_Compras` int(11) NOT NULL,
+  `Fecha` date NOT NULL,
+  `Factura` int(11) NOT NULL,
+  `ID_Personal` int(11) NOT NULL,
+  `ID_Proveedor` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `personal`
+--
+
+CREATE TABLE `personal` (
+  `Nombre` varchar(100) NOT NULL,
+  `DNI` varchar(9) NOT NULL,
+  `ID_Personal` int(11) NOT NULL,
+  `Direccion` varchar(200) NOT NULL,
+  `Email` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `Nombre` varchar(100) NOT NULL,
+  `Descripcion` varchar(500) NOT NULL,
+  `Precio` int(11) NOT NULL,
+  `Stock` int(11) NOT NULL,
+  `ID_Producto` int(11) NOT NULL,
+  `ID_Proveedor` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos_ventas`
+--
+
+CREATE TABLE `productos_ventas` (
+  `ID` int(11) NOT NULL,
+  `ID_Poducto` int(11) NOT NULL,
+  `ID_Venta` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedores`
+--
+
+CREATE TABLE `proveedores` (
+  `Nombre` varchar(100) NOT NULL,
+  `Direccion` varchar(200) NOT NULL,
+  `Email` varchar(200) NOT NULL,
+  `ID_Proveedor` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedores_productos`
+--
+
+CREATE TABLE `proveedores_productos` (
+  `ID` int(11) NOT NULL,
+  `ID_Poducto` int(11) NOT NULL,
+  `ID_Proveedor` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `telefonos`
+--
+
+CREATE TABLE `telefonos` (
+  `ID_Telefono` int(11) NOT NULL,
+  `Numero` int(11) NOT NULL,
+  `ID_Proveedor` int(11) DEFAULT NULL,
+  `ID_Personal` int(11) DEFAULT NULL,
+  `ID_Cliente` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ventas`
+--
+
+CREATE TABLE `ventas` (
+  `ID_Ventas` int(11) NOT NULL,
+  `Fecha` date NOT NULL,
+  `Factura` int(11) NOT NULL,
+  `ID_Cliente` int(11) NOT NULL,
+  `ID_Personal` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`ID_Cliente`);
+
+--
+-- Indices de la tabla `compras`
+--
+ALTER TABLE `compras`
+  ADD PRIMARY KEY (`ID_Compras`),
+  ADD KEY `fk_compras_personal` (`ID_Personal`),
+  ADD KEY `fk_compras_proveedor` (`ID_Proveedor`);
+
+--
+-- Indices de la tabla `personal`
+--
+ALTER TABLE `personal`
+  ADD PRIMARY KEY (`ID_Personal`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`ID_Producto`),
+  ADD KEY `fk_productos_proveedores` (`ID_Proveedor`);
+
+--
+-- Indices de la tabla `productos_ventas`
+--
+ALTER TABLE `productos_ventas`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `fk_ProductoVenta` (`ID_Poducto`),
+  ADD KEY `fk_VentaProducto` (`ID_Venta`);
+
+--
+-- Indices de la tabla `proveedores`
+--
+ALTER TABLE `proveedores`
+  ADD PRIMARY KEY (`ID_Proveedor`);
+
+--
+-- Indices de la tabla `proveedores_productos`
+--
+ALTER TABLE `proveedores_productos`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `fk_ProductosProveedores` (`ID_Poducto`),
+  ADD KEY `fk_ProveedoresProductos` (`ID_Proveedor`);
+
+--
+-- Indices de la tabla `telefonos`
+--
+ALTER TABLE `telefonos`
+  ADD PRIMARY KEY (`ID_Telefono`),
+  ADD KEY `fk_telefonos_clientes` (`ID_Cliente`),
+  ADD KEY `fk_telefonos_proveedores` (`ID_Proveedor`),
+  ADD KEY `fk_telefonos_personal` (`ID_Personal`);
+
+--
+-- Indices de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  ADD PRIMARY KEY (`ID_Ventas`),
+  ADD KEY `fk_ventas_clientes` (`ID_Cliente`),
+  ADD KEY `fk_ventas_personal` (`ID_Personal`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `ID_Cliente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `compras`
+--
+ALTER TABLE `compras`
+  MODIFY `ID_Compras` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `personal`
+--
+ALTER TABLE `personal`
+  MODIFY `ID_Personal` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `ID_Producto` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `proveedores`
+--
+ALTER TABLE `proveedores`
+  MODIFY `ID_Proveedor` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `proveedores_productos`
+--
+ALTER TABLE `proveedores_productos`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `telefonos`
+--
+ALTER TABLE `telefonos`
+  MODIFY `ID_Telefono` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  MODIFY `ID_Ventas` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `compras`
+--
+ALTER TABLE `compras`
+  ADD CONSTRAINT `fk_compras_personal` FOREIGN KEY (`ID_Personal`) REFERENCES `personal` (`ID_Personal`),
+  ADD CONSTRAINT `fk_compras_proveedor` FOREIGN KEY (`ID_Proveedor`) REFERENCES `proveedores` (`ID_Proveedor`);
+
+--
+-- Filtros para la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `fk_productos_proveedores` FOREIGN KEY (`ID_Proveedor`) REFERENCES `proveedores` (`ID_Proveedor`);
+
+--
+-- Filtros para la tabla `productos_ventas`
+--
+ALTER TABLE `productos_ventas`
+  ADD CONSTRAINT `fk_ProductoVenta` FOREIGN KEY (`ID_Poducto`) REFERENCES `productos` (`ID_Producto`),
+  ADD CONSTRAINT `fk_VentaProducto` FOREIGN KEY (`ID_Venta`) REFERENCES `ventas` (`ID_Ventas`);
+
+--
+-- Filtros para la tabla `proveedores_productos`
+--
+ALTER TABLE `proveedores_productos`
+  ADD CONSTRAINT `fk_ProductosProveedores` FOREIGN KEY (`ID_Poducto`) REFERENCES `productos` (`ID_Producto`),
+  ADD CONSTRAINT `fk_ProveedoresProductos` FOREIGN KEY (`ID_Proveedor`) REFERENCES `proveedores` (`ID_Proveedor`);
+
+--
+-- Filtros para la tabla `telefonos`
+--
+ALTER TABLE `telefonos`
+  ADD CONSTRAINT `fk_telefonos_clientes` FOREIGN KEY (`ID_Cliente`) REFERENCES `clientes` (`ID_Cliente`),
+  ADD CONSTRAINT `fk_telefonos_personal` FOREIGN KEY (`ID_Personal`) REFERENCES `personal` (`ID_Personal`),
+  ADD CONSTRAINT `fk_telefonos_proveedores` FOREIGN KEY (`ID_Proveedor`) REFERENCES `proveedores` (`ID_Proveedor`);
+
+--
+-- Filtros para la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  ADD CONSTRAINT `fk_ventas_clientes` FOREIGN KEY (`ID_Cliente`) REFERENCES `clientes` (`ID_Cliente`),
+  ADD CONSTRAINT `fk_ventas_personal` FOREIGN KEY (`ID_Personal`) REFERENCES `personal` (`ID_Personal`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

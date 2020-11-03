d9e524d719c9e877e28f3e256de4a23c3c74b355
Juan Fernando Diaz <59884391+JuanFernandoDiaz1@users.noreply.github.com>
2020-11-03 16:18:47 +0100

    imagenes

--- DiWi/src/main/java/controlador/Home.java
+++ DiWi/src/main/java/controlador/Home.java
@@ -6,6 +6,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Toolkit;
 
 public class Home extends JFrame {
 
@@ -31,6 +32,8 @@ public class Home extends JFrame {
 	 * Create the frame.
 	 */
 	public Home() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\juanfernando.diaz.JUANXXIII_23\\Documents\\GitHub\\SGE\\DiWi\\img\\logo-diwi.PNG"));
+		setTitle("DiWi");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
